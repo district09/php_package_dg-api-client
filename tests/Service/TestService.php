5b20132fb06454b3f57b38c17a7e4ab7ee9322a0
Peter Decuyper <peter.decuyper@digipolis.gent>
2019-12-16 13:43:18 +0100

    Backported the client() method

---
+++ tests/Service/TestService.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gent\Tests\API\Service;
+
+use DigipolisGent\API\Client\ClientInterface;
+use DigipolisGent\API\Service\ServiceAbstract;
+
+/**
+ * Test class to validate the ServiceAbstract.
+ *
+ * @codeCoverageIgnore
+ */
+class TestService extends ServiceAbstract
+{
+    /**
+     * Method only for testing purposes.
+     *
+     * @return \DigipolisGent\API\Client\ClientInterface
+     */
+    public function getClient(): ClientInterface
+    {
+        return $this->client();
+    }
+}
